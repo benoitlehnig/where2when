ff7c4051c677dd64b5a3abc229c3fb2ee1355175
benoitlehnig <benoit.lehnig@gmail.com>
2017-06-11 00:09:19 +0200

    authentication

--- where2when/public/app/app.module.js
+++ where2when/public/app/app.module.js
@@ -2,5 +2,9 @@
     /*global localStorage: true, console: true, $: true */
    'use strict';
     var myApp = angular.module('sampleApp', ['firebase','header','ngMaterial'])
+    .run(['$rootScope', '$timeout', function ($rootScope,$timeout) {
+
+   		$rootScope.firebaseUser = null;
+   	}]);
    
 }());
--- where2when/public/app/header/header.component.js
+++ where2when/public/app/header/header.component.js
@@ -9,5 +9,41 @@
 		}
 	);
 
-	function HeaderController($scope) {};
+	function HeaderController($scope,$firebaseAuth,$rootScope,$timeout) {
+		$scope.auth = $firebaseAuth();
+		$scope.firebaseUser = null;
+
+
+		$scope.signIn = function(){
+			var provider = new firebase.auth.GoogleAuthProvider();
+	        $scope.auth.$signInWithPopup(provider)
+	        .then(
+	        	function(firebaseUser) {
+	        		console.log(firebaseUser);
+	            	$rootScope.firebaseUser = firebaseUser;
+	            	$scope.firebaseUser= firebaseUser;
+	         })
+	        .catch(function(error) {
+	           		$scope.error = error;
+	        });
+		};
+
+		// any time auth state changes, add the user data to scope
+	    $scope.auth.$onAuthStateChanged(function(firebaseUser) {
+	      	if(firebaseUser !==null){
+	      		console.log(firebaseUser);
+	        	$rootScope.firebaseUser = firebaseUser;
+	        	$scope.firebaseUser= firebaseUser;
+	        	var profilePicUrl = firebaseUser.photoURL;
+	        	$('#user-pic')[0].style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
+	      	}	
+	      	else{
+	        
+	        	$rootScope.firebaseUser = null;
+	        	$timeout(function(){
+              		$scope.$apply();
+            	});
+	      	}
+	    });
+	  }
 }());
