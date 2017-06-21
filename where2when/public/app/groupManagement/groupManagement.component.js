22d82d6818cc1a22652ec0dbb76820b4e479510e
lricci <laurent.ricci@amadeus.com>
2017-06-21 15:57:40 +0200

    display of form

--- where2when/public/app/groupManagement/groupManagement.component.js
+++ where2when/public/app/groupManagement/groupManagement.component.js
@@ -13,9 +13,9 @@
 		$scope.refListGroup = firebase.database().ref("groups");
 
 		$scope.groups = $firebaseArray($scope.refListGroup);
-		$scope.newGroup={
-			people:[
-				{people1: "toto@gmail.com"}
+		$scope.newGroup = {
+			name: "",
+			people: [
 			]
 		};
 
@@ -23,23 +23,29 @@
 			$scope.newGroup.admin = $rootScope.firebaseUser.uid;
 			$scope.groups.$add($scope.newGroup);
 
-		$http({
-		    method: 'POST',
-		    url: "sendMail.php",
-		    data: "email=" + $scope.newGroup.people[0].email + "&groupName="+ $scope.newGroup.name,
-		    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		}).then(
-				function(response) {
-					//First function handles success
-					$scope.content = response.data;
-					alert(response.data);
-				}, function(response) {
-					//Second function handles error
-					$scope.content = "Something went wrong";
-					console.log(response);
-				}
-			);
+			$http({
+			    method: 'POST',
+			    url: "sendMail.php",
+			    data: "email=" + $scope.newGroup.people[0].email + "&groupName="+ $scope.newGroup.name,
+			    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).then(
+					function(response) {
+						//First function handles success
+						$scope.content = response.data;
+						alert(response.data);
+					}, function(response) {
+						//Second function handles error
+						$scope.content = "Something went wrong";
+						console.log(response);
+					}
+				);
 		};
+
+		$scope.addSomeone = function() {
+		    var someone = $scope.someone;
+		    $scope.someone = {}; // Do this to clean up the form fields
+		    $scope.newGroup.people.push(someone);
+	    };
 
 
 	  }
