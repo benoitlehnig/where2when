21ee8115b5a4c450fee29ebe66c5cc15c08e7714
lricci <laurent.ricci@amadeus.com>
2017-07-11 12:58:23 +0200

    add icons

--- where2when/public/app/applicationList/applicationList.component.js
+++ where2when/public/app/applicationList/applicationList.component.js
@@ -10,32 +10,32 @@
 	);
 
 	function ApplicationlistController($scope, $rootScope) {
+		// >0 means it is a real application
 		$rootScope.applicationList = [
 			{
 				id: 0,
 				title: "YOP",
+				icon: "YOP.png",
 				description: "Where are you going to eat ?",
 				path: 'oneApplication/yop'
 			},
 			{
 				id: 1,
 				title: "PlanE",
+				icon: "PlanE.png",
 				description: "Where are you going to travel ?"
 			},
 			{
 				id: 2,
 				title: "Mapikid",
+				icon: "Mapikid.png",
 				description: "Where are your children going to play ? With toilets not so far"
 			},
 			{
 				id: -1,
 				title: "Other ?",
+				icon: "Other.png",
 				description: "What do you want us to help you with ?"
-			},
-			{
-				id: -2,
-				title: "",
-				description: ". . ."
 			}
 		];
 	}
